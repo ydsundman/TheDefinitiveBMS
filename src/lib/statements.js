9dcde01293f11168a6c7ea0cfd215fe390775b92
yassal <yassal@yds.se>
2018-05-25 19:22:03 +0200

    Update statements.js
    Oops... fixed a nonsensical question

--- src/lib/statements.js
+++ src/lib/statements.js
@@ -1,7 +1,7 @@
 export default [
   {text: 'Do you absolutely have to fix this bug ASAP?', fix: true},
   {text: 'Are you prepared to fix this bug right now?', fix: true},
-  {text: 'Is bug is so old that you don\'t even understand what it means anymore?', fix: false},
+  {text: 'Is this bug so old that you don\'t even understand what it means anymore?', fix: false},
   {text: 'Oh, you don\'t feel like fixing this bug, so you\'re just going to let it hang around for a few more sprints?', fix: false},
   {text: 'You should fix this bug, but you\'re not prepared to prioritize it?', fix: false},
   {text: 'I see! You need to fix this bug, but you just want to fix it later?', fix: false},
