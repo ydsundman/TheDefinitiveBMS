a0688fb783ce75f55c7d630f26ec768316aadc9f
yassal <yassal@yds.se>
2018-05-25 19:26:40 +0200

    Update statements.js
    Shorter, catchier, question.

--- src/lib/statements.js
+++ src/lib/statements.js
@@ -2,7 +2,7 @@ export default [
   {text: 'Do you absolutely have to fix this bug ASAP?', fix: true},
   {text: 'Are you prepared to fix this bug right now?', fix: true},
   {text: 'Is this bug so old that you don\'t even understand what it means anymore?', fix: false},
-  {text: 'Oh, you don\'t feel like fixing this bug, so you\'re just going to let it hang around for a few more sprints?', fix: false},
+  {text: 'Oh, you\'re holding on to the bug because it might come in handy someday?', fix: false},
   {text: 'You should fix this bug, but you\'re not prepared to prioritize it?', fix: false},
   {text: 'I see! You need to fix this bug, but you just want to fix it later?', fix: false},
   {text: 'Ok, let me get this straight, you really do want to fix it?', fix: true},
